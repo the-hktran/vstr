2acb7c42444a20b8f983e67cc26df76c1847e944
the-hktran <big_hen@live.com>
2022-11-02 21:09:15 -0400

    Removed the separate vscf cpp files

--- cpp_wrappers/vscf_functions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-g++ -fPIC --shared -std=c++11 -Wall -O3 vscf_functions.cpp vscf_functions_wrapper.cpp \
-    -I${CONDA_PREFIX}/include \
-    -I${CONDA_PREFIX}/include/python3.10 \
-    -I${CONDA_PREFIX}/include/eigen3 \
-    -I/home/henry/include/spectra-0.6.2/include \
-    -o vscf_functions.so
