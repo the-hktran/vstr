Scaled dipole derivatives to have consistent units and fixed AInv approximation by including diagonal of H

ff/normal_modes.py
@@ -201,7 +201,7 @@ def GetHessian(mf, Method = 'rhf', MassWeighted = False, isotope_avg=True):
         return H
     elif Method == 'rks':
         mf.verbose = 0
-        HRaw = hessian.rks.Hessian(mf).kernel()
+        HRaw = mf.Hessian().kernel() #hessian.rks.Hessian(mf).kernel()
         if MassWeighted:
             H = np.einsum('pqxy,p,q->pqxy', HRaw, mass**-0.5, mass**-0.5)
         else:
@@ -243,8 +243,8 @@ if __name__ == '__main__':
     mf = scf.RHF(mol)
     mf.kernel()
     
-    w, C = GetNormalModes(mf, Method = 'ccsd_t')
-    print(w)
+    #w, C = GetNormalModes(mf, Method = 'ccsd_t')
+    #print(w)
 
     mdft = mol.KS()
     mdft.xc = 'b3lyp'

spectra/dipole.py
@@ -3,16 +3,38 @@ from pyscf import gto, scf, hessian
 from vstr.ff.force_field import PerturbCoord
 from vstr.ff.normal_modes import CoordToAtom, AtomToCoord
 from vstr.cpp_wrappers.vhci_jf.vhci_jf_functions import FConst
+from vstr.utils import constants
 
 def MakeDipoleList(mu_raw):
     DipoleSurface = []
     for m in mu_raw:
-        d = FConst(m[0], m[1], True)
+        d = FConst(m[0] * np.sqrt(2**len(m[1])), m[1], True)
         DipoleSurface.append(d)
     return DipoleSurface
 
 def DipoleNorm(mf):
     return np.sqrt((mf.dip_moment()**2).sum())
+
+def EthansDipole(mf):
+    ao_dip = mf.mol.intor_symmetric('int1e_irp')
+    e1_dip = np.einsum('xij,ji->x', ao_dip, mf.make_rdm1())
+
+    charges = mol.atom_charges()
+    coords = mol.atom_coords()
+    nucl_dip = np.einsum('i,ix->x', charges, coords)
+    mol_dip = e1_dip #nucl_dip - e1_dip
+
+    from pyscf.data import nist
+    return mol_dip *2.541746 
+
+def ScaleDipole(mu, Freq, Modes):
+    n = len(Modes)
+    ScaledMu = mu / ((2 * np.pi)**(n / 2) * constants.AMU_TO_ME**(n / 2) * constants.C_AU**(n / 2) * constants.BOHR_TO_CM**(n / 2))
+    if Freq is not None:
+        for i in Modes:
+            ScaledMu = ScaledMu / np.sqrt(Freq[i])
+    # Units are now scaled by unitless factor. Dipole derivative has the same units as dipole.
+    return ScaledMu
 
 def PerturbDipole(X0, Modes, Coords, dx, atom0, mol, Method = 'rhf'):
     X = PerturbCoord(X0, Modes, Coords, dx)
@@ -23,7 +45,7 @@ def PerturbDipole(X0, Modes, Coords, dx, atom0, mol, Method = 'rhf'):
     new_mf.kernel(verbose = 0)
     return DipoleNorm(new_mf)
 
-def GetDipoleSurface(mf, Coords, Order = 1, dx = 1e-1):
+def GetDipoleSurface(mf, Coords, Freq = None, Order = 1, dx = 1e-1):
     Dipole = []
     NCoord = Coords.shape[1]
     X0 = AtomToCoord(mf) # in Bohr
@@ -45,19 +67,23 @@ def GetDipoleSurface(mf, Coords, Order = 1, dx = 1e-1):
             mu_m = PerturbDipole(X0, [[i, -1]], Coords, dx, atom0, new_mol)
 
             mu_i = (mu_p - mu_m) / (2 * dx)
+            mu_i = ScaleDipole(mu_i, Freq, [i])
             mu1.append((mu_i, [i]))
 
             if Order >= 2:
                 mu_ii = (mu_p + mu_m - 2 * mu0) / (dx * dx)
+                mu_ii = ScaleDipole(mu_ii, Freq, [i, i])
                 mu2.append((mu_ii, [i, i]))
 
             if Order >= 3:
                 mu_pp = PerturbDipole(X0, [[i, 2]], Coords, dx, atom0, new_mol)
                 mu_mm = PerturbDipole(X0, [[i, -2]], Coords, dx, atom0, new_mol)
                 mu_iii = (mu_pp - 2 * mu_p + 2 * mu_m - mu_mm) / (2 * dx * dx * dx)
+                mu_iii = ScaleDipole(mu_iii, Freq, [i, i, i])
                 mu3.append((mu_iii, [i, i, i]))
                 if Order >= 4:
                     mu_iiii = (mu_pp - 4 * mu_p + 6 * mu0 - 4 * mu_m + mu_mm) / (dx**4)
+                    mu_iiii = ScaleDipole(mu_iiii, Freq, [i, i, i, i])
                     mu4.append((mu_iiii, [i, i, i, i]))
 
         Dipole.append(mu1)
@@ -70,6 +96,7 @@ def GetDipoleSurface(mf, Coords, Order = 1, dx = 1e-1):
                 mu_mp = PerturbDipole(X0, [[i, -1], [j, 1]], Coords, dx, atom0, new_mol)
                 mu_mm = PerturbDipole(X0, [[i, -1], [j, -1]], Coords, dx, atom0, new_mol)
                 mu_ij = (mu_pp + mu_mm - mu_pm - mu_mp) / (4 * dx * dx)
+                mu_ij = ScaleDipole(mu_ij, Freq, [i, j])
                 mu2.append((mu_ij, [i, j]))
         Dipole.append(mu2)
 
@@ -84,6 +111,7 @@ def GetDipoleSurface(mf, Coords, Order = 1, dx = 1e-1):
                 mu_mmm = PerturbDipole(X0, [[i, -2], [j, -1]], Coords, dx, atom0, new_mol)
                 mu_00m = PerturbDipole(X0, [[j, -1]], Coords, dx, atom0, new_mol)
                 mu_iij = (mu_ppp + mu_mmp - 2 * mu_00p - mu_ppm - mu_mmm + 2 * mu_00m) / (8 * dx**3)
+                mu_iij = ScaleDipole(mu_iij, Freq, [i, i, j])
                 mu3.append((mu_iij, [i, i, j]))
                 for k in range(j + 1, NCoord):
                     mu_ppp = PerturbDipole(X0, [[i, 1], [j, 1], [k, 1]], Coords, dx, atom0, new_mol)
@@ -95,6 +123,7 @@ def GetDipoleSurface(mf, Coords, Order = 1, dx = 1e-1):
                     mu_mmp = PerturbDipole(X0, [[i, -1], [j, -1], [k, 1]], Coords, dx, atom0, new_mol)
                     mu_mmm = PerturbDipole(X0, [[i, -1], [j, -1], [k, -1]], Coords, dx, atom0, new_mol)
                     mu_ijk = (mu_ppp - mu_ppm - mu_pmp - mu_mpp + mu_mmp + mu_mpm + mu_pmm - mu_mmm) / (8 * dx**3)
+                    mu_ijk = ScaleDipole(mu_ijk, Freq, [i, j, k])
                     mu3.append((mu_ijk, [i, j, k]))
         Dipole.append(mu3)
 
@@ -112,6 +141,7 @@ def GetDipoleSurface(mf, Coords, Order = 1, dx = 1e-1):
                 mu_mmmm = PerturbDipole(X0, [[i, -3], [j, -1]], Coords, dx, atom0, new_mol)
                 mu_m00m = PerturbDipole(X0, [[i, -1], [j, -1]], Coords, dx, atom0, new_mol)
                 mu_iiij = (mu_pppp - 3 * mu_p00p + 3 * mu_m00p - mu_mmmp - mu_pppm + 3 * mu_p00m - 3 * mu_m00m + mu_mmmm) / (16 * dx**4)
+                mu_iiij = ScaleDipole(mu_ij, Freq, [i, i, i, j])
                 mu4.append((mu_iiij, [i, i, i, j]))
                 for k in range(j + 1, NCoord):
                     #iijk terms
@@ -128,6 +158,7 @@ def GetDipoleSurface(mf, Coords, Order = 1, dx = 1e-1):
                     mu_mmmm = PerturbDipole(X0, [[i, -2], [j, -1], [k, -1]], Coords, dx, atom0, new_mol)
                     mu_00mm = PerturbDipole(X0, [[j, -1], [k, -1]], Coords, dx, atom0, new_mol)
                     mu_iijk = (mu_pppp + mu_mmpp - 2 * mu_00pp - mu_ppmp - mu_mmmp + 2 * mu_00mp - mu_pppm - mu_mmpm + 2 * mu_00pm + mu_ppmm + mu_mmmm - 2 * mu_00mm) / (16 * dx**4)
+                    mu_iijk = ScaleDipole(mu_iijk, Freq, [i, i, j, k])
                     mu4.append((mu_iijk, [i, i, j, k]))
                     for l in range(k + 1, NCoord):
                         #ijkl terms
@@ -148,6 +179,7 @@ def GetDipoleSurface(mf, Coords, Order = 1, dx = 1e-1):
                         mu_pmmm = PerturbDipole(X0, [[i, 1], [j, -1], [k, -1], [l, -1]], Coords, dx, atom0, new_mol)
                         mu_mmmm = PerturbDipole(X0, [[i, -1], [j, -1], [k, -1], [l, -1]], Coords, dx, atom0, new_mol)
                         mu_ijkl = (mu_pppp - mu_mppp - mu_pmpp + mu_mmpp - mu_ppmp + mu_mpmp + mu_pmmp - mu_mmmp - mu_pppm + mu_mppm + mu_pmpm - mu_mmpm + mu_ppmm - mu_mpmm - mu_pmmm + mu_mmmm) / (16 * dx**4)
+                        mu_ijkl = ScaleDipole(mu_ijkl, Freq, [i, j, k, l])
                         mu4.append((mu_ijkl, [i, j, k, l]))
         Dipole.append(mu4)
 
@@ -171,8 +203,10 @@ if __name__ == "__main__":
     mf = scf.RHF(mol)
     mf.kernel()
 
-    w, C = GetNormalModes(mf, Method = 'rhf')
+   # w, C = GetNormalModes(mf, Method = 'rhf')
 
-    mu = GetDipoleSurface(mf, C, Order = 2)
-    print(mu)
+    #mu = GetDipoleSurface(mf, np.eye(21), Order = 1)
+    #print(mu)
+    emu = EthansDipole(mf)
+    print(emu)
     

spectra/ir_lr.py
@@ -8,6 +8,8 @@ def GetTransitionDipoleMatrix(mIR, IncludeZeroth = True):
     mIR.D = GenerateHamAnharmV(mIR.mVCI.Basis, list(mIR.mVCI.Frequencies), mIR.DipoleSurfaceList, mIR.DipoleSurface[3], mIR.DipoleSurface[4], mIR.DipoleSurface[5], mIR.DipoleSurface[6])
     if IncludeZeroth:
         mIR.D += np.eye(mIR.D.shape[0]) * mIR.DipoleSurface[0][0]
+    else:
+        np.fill_diagonal(mIR.D, 0)
 
 def GetTransitionDipoleMatrixFromVSCF(mIR, IncludeZeroth = True):
     X0 = []
@@ -33,12 +35,29 @@ def GetAb(mIR, w, Basis = None):
 def SolveAxb(A, b):
     return np.linalg.solve(A, b)
 
+def ApproximateAInv(mIR, w, Order = 1):
+    HOD = mIR.mVCI.H.todense()
+    D = np.asarray(HOD.diagonal().copy() + (w + mIR.mVCI.E[0] + 1.j * mIR.eta)).ravel()
+    DInv = D**(-1)
+    np.fill_diagonal(HOD, 0)
+    
+    AInv = np.diag(DInv) + np.einsum('ij,i,j->ij', HOD, DInv, DInv, optimize = True)
+    if Order >= 2:
+        HDH = np.einsum('ij,j,jk->ik', HOD, DInv, HOD, optimize = True)
+        AInv += 0.5 * np.einsum('ij,i,j->ij', HDH, DInv, DInv, optimize = True)
+    return AInv
+
 def Intensity(mIR, w):
     # Should define new basis with HCI and then solve VHCI here, be sure to update mVCI object
     A, b = mIR.GetAb(w)
     x = SolveAxb(A, b)
+    #AInv = mIR.ApproximateAInv(w)
+    #AInvX = np.linalg.inv(A)
+    #print(w, ((AInv - AInvX).conj() * (AInv - AInvX)).sum().real)
+    #x = AInv @ b
+    x = np.asarray(x).ravel()
+    b = np.asarray(b).ravel()
     return (1.j * np.dot(b, x)).real / np.pi
-    #return (np.dot(b, x)).imag
 
 def PlotSpectrum(mIR, PlotName, XLabel = "Frequency", YLabel = "Intensity", Title = "IR Spectrum"):
     plt.plot(mIR.ws, mIR.Is, linestyle = '-', marker = None)
@@ -50,13 +69,16 @@ def PlotSpectrum(mIR, PlotName, XLabel = "Frequency", YLabel = "Intensity", Titl
 def TestPowerSeries(mIR):
     H = mIR.mVCI.H.todense()
     n = H.shape[0]
-    for w in [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]:
-        D = np.eye(n) * (w + mIR.mVCI.E[0] + mIR.eta * 1.j)
+    for w in (mIR.mVCI.E - mIR.mVCI.E[0]): #range(1000,16000,1):
+        D = np.diag(H.diagonal().copy()) + np.eye(H.shape[0]) * (w + mIR.mVCI.E[0] + mIR.eta * 1.j)
+        np.fill_diagonal(H, 0)
         DInv = np.linalg.inv(D)
         AInvX = np.linalg.inv(D - H)
         AInvP = DInv + DInv @ H @ DInv
-        np.save("AInvX", AInvX)
-        np.save("AInvP", AInvP)
+        #print(AInvP)
+        #print(AInvX)
+        #np.save("AInvP", AInvP)
+        #np.save("AInvX", AInvX)
         dA = AInvX - AInvP
         err = (dA.conj() * dA).sum()
         print("err", w, err.real)
@@ -65,6 +87,7 @@ class LinearResponseIR:
     GetAb = GetAb
     GetTransitionDipoleMatrix = GetTransitionDipoleMatrix
     Intensity = Intensity
+    ApproximateAInv = ApproximateAInv
     PlotSpectrum = PlotSpectrum
 
     TestPowerSeries = TestPowerSeries
@@ -88,7 +111,7 @@ class LinearResponseIR:
     def kernel(self):
         # Make dipole surface
         if self.DipoleSurface is None:
-            mu_raw = GetDipoleSurface(self.mf, self.NormalModes, Order = self.Order, dx = 1e-1)
+            mu_raw = GetDipoleSurface(self.mf, self.NormalModes, Freq = self.Frequencies, Order = self.Order, dx = 1e-1)
             print(mu_raw)
             self.DipoleSurface = []
             self.DipoleSurface.append(mu_raw[0][0])
@@ -138,6 +161,8 @@ if __name__ == "__main__":
     
     mVHCI = VHCI(w, V, MaxQuanta = 10, MaxTotalQuanta = 1, eps1 = 100, eps2 = 0.001, eps3 = -1, NWalkers = 50, NSamples = 50, NStates = 1)
     mVHCI.kernel()
+    mVHCI.E, mVHCI.C = np.linalg.eigh(mVHCI.H.todense())
+    mVHCI.E_HCI = mVHCI.E
 
     mIR = LinearResponseIR(mf, mVHCI, FreqRange = [0, 16000], NPoints = 1000, eta = 100, NormalModes = NormalModes, Order = 4)
     mIR.kernel()